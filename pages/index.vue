a6bccb2aaa52154cb6b5b1b95ae396b51a56e6b2
unknown <lrgfilipe@gmail.com>
2020-03-21 19:58:45 +0000

    app title

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col">
     <div class="border-b-2 border-white flex pb-4 flex-shrink-0">
       <div class="w-1/2 flex items-center justify-center font-medium text-2xl text-white">
-        App Name
+        Triangles
       </div>
       <div class="w-1/2 text-center font-medium text-white rounded-lg text-lg bg-orange-500">
         <div class="border-b border-white py-1">
