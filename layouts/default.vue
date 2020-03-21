9c0e2fa090eacb6ee329a59ee4ad2034ee4eb882
unknown <lrgfilipe@gmail.com>
2020-03-21 19:36:43 +0000

    WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="w-full h-screen flex items-center justify-center bg-orange-400 sm:bg-gray-200">
-    <div class="w-full sm:w-app bg-orange-400 sm:rounded-lg sm:shadow-lg p-4 text-gray-900">
-      <nuxt />
+  <div class="w-full h-screen bg-orange-400 sm:bg-gray-200">
+    <div class="sm:h-screen flex items-center justify-center">
+      <div class="w-full sm:w-app bg-orange-400 sm:rounded-lg sm:shadow-lg p-4 text-gray-900">
+        <nuxt />
+      </div>
     </div>
   </div>
 </template>
